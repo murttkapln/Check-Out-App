8b1d33995af3d756515d9cd30062e51a6d68a8ba
Murat <murttkapln@gmail.com>
2023-06-13 20:41:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// const textRate = 0.18;
+// const shippingPrice = 25.99;
+// const freeShippingPrice = 3000;
+
+
+//* localStorage vs. sessionStorage
+window.addEventListener("load", ()=> {
+    // localStorage.setItem("taxRate", taxRate)
+    // localStorage.setItem("shippingPrice", shippingPrice)
+    // localStorage.setItem("freeShippingPrice", freeShippingPrice)
+});
+
+const navbarList = document.querySelector(".nav__list");
+const productList = document.querySelector("div.main__product-painel");
